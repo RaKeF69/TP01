ffc1758bb92c4ca8f5bcd95f53366594db3d258e
RaKeF69 <71253146+RaKeF69@users.noreply.github.com>
2020-10-05 12:01:03 +0200

    Update tp01_ex4.py

--- tp01_ex4.py
+++ tp01_ex4.py
@@ -15,9 +15,21 @@ Résultats : Un message spécifiant si la date entrée est valide.
 """
 
 ### Déclaration et initialisation des variables
-
+MESSAGE1: str = "Cette date est valide"
+MESSAGE2: str = "Cette date n'est pas valide"
+MOIS_31: list = [1, 3, 5, 7, 8, 10, 12]
+jour_uti: int = None
+mois_uti: int = None
+annee_uti: int = None
 
 ### Séquence d'opération
-
-
-        
+jour_uti = int(input("Saisissez un jour: "))
+mois_uti = int(input("Sasissez un mois: "))
+annee_uti = int(input("Saisissez une année: "))
+if mois_uti > 13 or jour_uti > 31:
+    print(MESSAGE2)
+elif jour_uti == 31:
+    if mois_uti in list(MOIS_31):
+        print(MESSAGE1)
+elif mois_uti == 2:
+    if jour_uti > 28
