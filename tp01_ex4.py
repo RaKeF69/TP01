eb5e4604703c1322a766450ace4ba99ed2127041
RaKeF69 <71253146+RaKeF69@users.noreply.github.com>
2020-10-05 22:52:47 +0200

    Update tp01_ex4.py

--- tp01_ex4.py
+++ tp01_ex4.py
@@ -18,6 +18,11 @@ Résultats : Un message spécifiant si la date entrée est valide.
 MESSAGE1: str = "Cette date est valide"
 MESSAGE2: str = "Cette date n'est pas valide"
 MOIS_31: list = [1, 3, 5, 7, 8, 10, 12]
+MOIS_MAX: int = 13
+JOURS_MAX: int = 31
+FEVRIER: int = 2
+JOURS_MAX_FEVRIER: int = 29
+ANNEE_BISS: list = [4, 100, 400]
 jour_uti: int = None
 mois_uti: int = None
 annee_uti: int = None
@@ -29,7 +34,7 @@ mois_uti = int(input("Sasissez un mois: "))
 annee_uti = int(input("Saisissez une année: "))
 ### Si les mois sont supérieur à 13 ou les jours supérieur à 31, on affiche le message d'erreur.
 ### Par soucis d'efficacité, la vérification pour les années bisextiles est exécutée en même temps
-if mois_uti > 13 or jour_uti > 31 or (jour_uti == 31 and mois_uti not in list(MOIS_31)) or (mois_uti == 2 and (jour_uti > 29 or not(jour_uti == 29 and ((annee_uti % 4 == 0 and annee_uti % 100 != 0) or annee_uti % 400 == 0)))):
+if mois_uti > MOIS_MAX or jour_uti > JOURS_MAX or (jour_uti == JOURS_MAX and mois_uti not in list(MOIS_31)) or (mois_uti == FEVRIER and (jour_uti > JOURS_MAX_FEVRIER or not(jour_uti == JOURS_MAX_FEVRIER and ((annee_uti % ANNEE_BISS[0] == 0 and annee_uti % ANNEE_BISS[1] != 0) or annee_uti % ANNEE_BISS[2] == 0)))):
     print(MESSAGE2)
 else:
     print(MESSAGE1)
